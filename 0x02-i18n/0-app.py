35f4fb33f84cd6a3428a0643709713b54b775107
Eman288 <emantamam107@gmail.com>
2024-11-05 23:51:31 +0000

    fix task 0

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     """a function to return the index page"""
-    return render_template("templates/0-index.html")
+    return render_template("0-index.html")
 
 if __name__ == '__main__':
     app.run()

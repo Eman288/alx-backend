4accb6a898d4e086ae8de67989ad8a473db3f60f
Eman288 <emantamam107@gmail.com>
2024-11-09 01:49:03 +0000

    task1

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from flask_babel import Babel
+"""
+a module to set a basic babel setup
+"""
+
+
+class Config :
+    LANGUAGES = ["en", "fr"]
+    DEFAULT_LOCALE = "en"
+    TIMEZONE = "UTC"
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
